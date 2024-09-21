7579ef8f2753ce40dcfaad81868ccacf3dd95922
vijackson <2455027913@qq.com>
2024-09-21 20:51:59 +0800

    first commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/ui_about.h
@@ -0,0 +1,60 @@
+/********************************************************************************
+** Form generated from reading UI file 'about.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ABOUT_H
+#define UI_ABOUT_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QTextBrowser>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_About
+{
+public:
+    QTextBrowser *textBrowser;
+
+    void setupUi(QDialog *About)
+    {
+        if (About->objectName().isEmpty())
+            About->setObjectName("About");
+        About->resize(280, 143);
+        textBrowser = new QTextBrowser(About);
+        textBrowser->setObjectName("textBrowser");
+        textBrowser->setGeometry(QRect(0, 0, 281, 151));
+
+        retranslateUi(About);
+
+        QMetaObject::connectSlotsByName(About);
+    } // setupUi
+
+    void retranslateUi(QDialog *About)
+    {
+        About->setWindowTitle(QCoreApplication::translate("About", "Dialog", nullptr));
+        textBrowser->setHtml(QCoreApplication::translate("About", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Microsoft YaHei UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">\345\274\200\345\217\221\350\200\205\357\274\232\346\234\261\345\276\267\346\265\251</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">2022414040244</p></body></html>", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class About: public Ui_About {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ABOUT_H
